522b69e3194f5709785f5206bef175fdedf2cd33
Dmitry O. Markov <markov.dm@inbox.ru>
2020-08-06 19:35:59 +0300

    add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_121233) do
+ActiveRecord::Schema.define(version: 2020_08_06_131704) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 2020_08_05_121233) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.string "password_hash"
+    t.string "password_salt"
+    t.string "avatar_url"
   end
 
 end
