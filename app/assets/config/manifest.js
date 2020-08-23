522f7e9db1c8e222ce816f4bcf752564635b9b27
Dmitry O. Markov <markov.dm@inbox.ru>
2020-08-23 03:58:35 +0300

    Views refactoring

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,4 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 
-$(function(){
-    $('#ask-button').click(function(){
-        $('#ask-form').slideToggle(300);
-        return false;
-    });
-});
+
