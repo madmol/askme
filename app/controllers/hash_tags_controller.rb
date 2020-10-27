6d8ab66899bdf93fec491e74d658710f0759425f
Dmitry O. Markov <markov.dm@inbox.ru>
2020-10-27 13:26:19 +0300

    updated logic for 404 error in HashTagsController#show

--- app/controllers/hash_tags_controller.rb
+++ app/controllers/hash_tags_controller.rb
@@ -1,17 +1,6 @@
 class HashTagsController < ApplicationController
-  before_action :load_tag
-
   def show
-    if @hash_tag.blank?
-      render_404
-    else
-      @questions = @hash_tag.questions
-    end
-  end
-
-  private
-
-  def load_tag
-    @hash_tag = HashTag.with_questions.find_by(name: params[:name])
+    @hash_tag = HashTag.with_questions.find_by!(name: params[:name])
+    @questions = @hash_tag.questions
   end
 end
